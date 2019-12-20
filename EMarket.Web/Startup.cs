28eeec385c5f3dfa231873f730207fcf93c734d4
medine ozkanli <ozknlimedine@gmail.com>
2019-12-20 21:15:25 +0300

    Session

--- EMarket.Web/Startup.cs
+++ EMarket.Web/Startup.cs
@@ -33,6 +33,7 @@ namespace EMarket.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+           
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -51,6 +52,18 @@ namespace EMarket.Web
             services.AddScoped(typeof(IRepository<>), typeof(EFRepository<>));
             services.AddScoped<IHomeIndexViewModelService, HomeIndexViewModelService>();
             services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                // Make the session cookie essential
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -81,6 +94,7 @@ namespace EMarket.Web
             app.UseCookiePolicy();
 
             app.UseAuthentication();
+            app.UseSession();
 
             app.UseMvc(routes =>
             {
